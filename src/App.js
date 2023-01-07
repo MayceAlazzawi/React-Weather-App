204bca216a770959e4d9a8b0e97ee727fb97251d
Mayce <mayce@Meenas-MacBook-Pro.local>
2023-01-07 22:14:14 +0300

    Making weather component shows city name for the first slide

--- src/App.js
+++ src/App.js
@@ -1,9 +1,9 @@
 import './App.css';
 import React, { useEffect, useState } from "react";
 import Weather from "./components/Weather";
-import { Splide, SplideTrack, SplideSlide } from '@splidejs/react-splide';
+import { Splide, SplideSlide } from '@splidejs/react-splide';
 import '@splidejs/splide/css/sea-green';
-import { findByLabelText } from '@testing-library/react';
+
 function App() {
   const [lat, setLat] = useState([]);
   const [long, setLong] = useState([]);
@@ -16,49 +16,51 @@ function App() {
         setLat(position.coords.latitude);
         setLong(position.coords.longitude);
       });
-
-      //Fetch weather data from weather api based on our latitude and longitude
-      await fetch(`https://api.openweathermap.org/data/2.5/weather/?lat=${lat}&lon=${long}&units=metric&APPID=f37b157d4e4a55815a943abd53f0d644  `)
+      // https:/  /pro.openweathermap.org/data/2.5/forecast/hourly?lat=44.34&lon=10.99&appid={API key}
+      // Fetch weather data from weather api based on our latitude and longitude
+      // https://api.openweathermap.org/data/2.5/forecast?
+      await fetch(`https://api.openweathermap.org/data/2.5/forecast?lat=${lat}&lon=${long}&appid=f37b157d4e4a55815a943abd53f0d644`)
         .then(res => res.json())
         .then(result => {
           setData(result)
-          console.log(data);
+          console.log(result);
         });
+
     }
     fetchData();
   }, [lat, long]);
-
-
+  console.log(data)
   return (
     <div className="App">
-      {(typeof data.main !== "undefined") ?
-        (
-          <Splide aria-labelledby="My Favorite Images" options={{
-            // rewind: true,
-            width: 1400,
-            gap: '1rem',
-            display: "flex",
-            // flex-direction: "row"
-          }}>
-            <SplideSlide >
-              {/* <SplideTrack> */}
-              <Weather weatherData={data} />
-              <Weather weatherData={data} />
-              <Weather weatherData={data} />
-              {/* </SplideTrack> */}
-            </SplideSlide>
+      {(typeof data.list !== "undefined") ?
+        (<Splide aria-labelledby="My Favorite Images" options={{ rewind: true, width: 1400, gap: "1rem", display: "flex" }}>
+          <SplideSlide> <Weather weatherData={data.list[0]} city={data.city.name} /></SplideSlide>
+          <SplideSlide>Istanbul's Weather:2</SplideSlide>
 
-            <SplideSlide >
-              {/* <SplideTrack> */}
-              <Weather weatherData={data} />
-              <Weather weatherData={data} />
-              {/* </SplideTrack> */}
-            </SplideSlide>
-
-
-          </Splide>
+        </Splide>
+          // <Splide aria-labelledby="My Favorite Images" options={{
+          //   rewind: true,
+          //   width: 1400,
+          //   gap: '1rem',
+          //   display: "flex",
+          //   // flex-direction: "row"
+          // }}>
+          //   <SplideSlide>
+          //     <SplideSlide>
+          //       <Weather weatherData={data.list[0]} />
+          //       <Weather weatherData={data.list[0]} city={data.city.name} />
+          //       <Weather weatherData={data.list[1]} city={data.city.name} />
+          //       <Weather weatherData={data.list[2]} city={data.city.name} />
+          //     </SplideSlide>
+          //   </SplideSlide>
+          //   <SplideSlide>
+          //     <Weather weatherData={data.list[3]} city={data.city.name} />
+          //     <Weather weatherData={data.list[4]} city={data.city.name} />
+          //     <Weather weatherData={data.list[5]} city={data.city.name} />
+          //   </SplideSlide>
+          // </Splide >
         ) : (
-          <div></div>
+          <div>Loading...</div>
         )
       }
     </div >
--- src/components/Weather.js
+++ src/components/Weather.js
@@ -8,29 +8,34 @@ const refresh = () => {
   window.location.reload();
 }
 
-const Weather = (weatherData) => {
-  console.log(weatherData.weatherData)
+const Weather = ({ weatherData, city }) => {
+  // console.log(weatherData[0].main.temp_max)
+
+  // let list = weatherData.weatherData.list;
+  console.log(weatherData)
 
   return (
     <div className="Card ">
       <div className="top">
-        <p className="header padding">{weatherData.weatherData.name}</p>
+        <p className="header padding">{city}</p>
         <Button className="button" inverted color='blue' circular icon='refresh' onClick={refresh} />
       </div>
-      <div className="flex color padding">
-        <p className="day color">{moment().format('dddd')}, <span>{moment().format('LL')}</span></p>
-        <p className="description  color">{weatherData.weatherData.weather[0].main}</p>
-      </div>
+      {/* <div className="flex color padding">
+        <p className="day color">{moment().format('dddd')},<span>{weatherData.weatherData[0].dt_txt}</span> </p>
+        <p className="description  color">{weatherData.weatherData[0].weather[0].main}</p> */}
+      {/* <div id="icon"><img id="wicon" src={`http://openweathermap.org/img/w/${weatherData.weatherData[0].weather[0].icon}.png`} alt="Weather icon" /></div> */}
+      {/* <img className="description  color" src={`${list[0].weather[0].icon}`} alt="weather">{list[0].weather[0].icon}</img> */}
+      {/* </div> */}
 
-      <div className="flex color padding">
-        <p className="temp color">Temprature: {weatherData.weatherData.main.temp} &deg;C</p>
-        <p className="temp color">Humidity: {weatherData.weatherData.main.humidity} %</p>
-      </div>
+      {/* <div className="flex color padding">
+       <p className="temp color">Temprature: {weatherData.weatherData[0].main.temp} &deg;C</p>
+        <p className="temp color">Humidity:  {weatherData.weatherData[0].main.humidity} %</p>
+       </div> */}
 
-      <div className="flex color padding">
+      {/* <div className="flex color padding">
         <p className="sunrise-sunset color">Sunrise: {new Date(weatherData.weatherData.sys.sunrise * 1000).toLocaleTimeString('en-IN')}</p>
         <p className="sunrise-sunset color">Sunset: {new Date(weatherData.weatherData.sys.sunset * 1000).toLocaleTimeString('en-IN')}</p>
-      </div>
+      </div>  */}
     </div>
 
   )
